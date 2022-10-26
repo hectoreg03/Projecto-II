0d003b9e5923e0c79fa3fe1b3b9243dcd0bc43b9
hectoreg03 <96084225+hectoreg03@users.noreply.github.com>
2022-10-25 23:47:12 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ function isArray(what) {
     return Object.prototype.toString.call(what) === '[object Array]';
 }
 var blogs=[];
+var ingredientes= [];
+var pasos=[];
 app.get('/',(req,res)=>{
     console.log(blogs.length);
     res.render("index.ejs", {blogs: blogs});
@@ -25,17 +27,55 @@ app.get('/aboutus',(req,res)=>{
 })
 
 app.get('/publicar',(req,res)=>{
-    res.render("publicar.ejs")
+    res.render("publicar.ejs",{steps: pasos, ingredients: ingredientes} );
 })
 
 app.post('/publicar',(req,res)=>{
     var title = req.body.recipeName;
     var notes = req.body.talkusabouttherecipe;
-    var ingredients = req.body.ingredients;
-    var preparation = req.body.stepstofollow;
-    blogs.push({title:title, notes:notes, ingredients:ingredients,preparation:preparation});
+    blogs.push({title:title, notes:notes, ingredients:ingredientes,preparation:pasos});
+    ingredientes=[];
+    pasos=[];
     console.log(blogs.length);
-    res.render("publicar.ejs")
+    res.render("publicar.ejs",{steps: pasos, ingredients: ingredientes} );
+})
+
+app.post('/publicar/Preparacion',(req,res)=>{
+    var paso = req.body.paso;
+    pasos.push(paso);
+    res.render("publicar.ejs",{steps: pasos, ingredients: ingredientes} );
+})
+
+app.post('/publicar/Ingredientes',(req,res)=>{
+    var ingredientein = req.body.ingrediente;
+    ingredientes.push(ingredientein);
+    console.log("Tamao Ingredientes ");
+    console.log(ingredientes.length);
+    res.render("publicar.ejs",{steps: pasos, ingredients: ingredientes} );
+})
+
+app.post('/publicar/dPreparacion',(req,res)=>{
+   var aux;
+    for(var i=req.body.x; i+1<pasos.length;i++ ){
+        aux=pasos[i];
+        pasos[i]=pasos[i+1];
+        pasos[i+1]=aux;
+    }
+    pasos.pop();
+    res.render("publicar.ejs",{steps: pasos, ingredients: ingredientes} );
+})
+
+app.post('/publicar/dIngredientes',(req,res)=>{
+    var aux;
+     for(var i=req.body.x; i+1<ingredientes.length;i++ ){
+         aux=ingredientes[i];
+         ingredientes[i]=ingredientes[i+1];
+         ingredientes[i+1]=aux;
+     }
+     ingredientes.pop();
+     console.log("Tamao Ingredientes ");
+     console.log(ingredientes.length);
+     res.render("publicar.ejs",{steps: pasos, ingredients: ingredientes} );
 })
 
 app.listen(3000,()=>{
